e6a9c0ba0eb3fc7957687b314c1c73140a4a1187
JoeBellWork <JoeBellWork@users.noreply.github.com>
2020-03-31 16:09:03 +0100

    Added game

---
+++ FranticFighter-10Seconds OR less/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    public float speed;
+    private Vector3 posZ;
+
+    private void Start()
+    {
+        posZ = transform.position;
+    }
+
+
+
+
+
+
+    void Update()
+    {
+
+
+        face();
+    }
+
+
+
+
+
+
+
+
+
+    void face()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        transform.position = Vector3.Lerp(transform.position, mousePos, speed * Time.deltaTime); //floaty
+        //transform.position = Vector3.MoveTowards(transform.position, mousePos, speed * Time.deltaTime); //not floaty
+
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        transform.up = direction;      
+    }
+}
---
+++ FranticFighter-10Seconds OR less/Assets/Scripts/Timer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public Image countDown;
+
+    bool changeColour = true;
+    float currentTime;
+    float startTime = 10f;
+    string gameOver = "Game Over";
+    public Color red;
+    public Color white;
+    public Color black;
+    
+
+    void Start()
+    {
+        currentTime = startTime;
+        countDown.fillAmount = (startTime/10);
+        
+    }
+
+    void Update()
+    {
+        if (currentTime >= 0.01)
+        {
+            currentTime -= 1 * Time.deltaTime;
+            timerText.text = currentTime.ToString("F2");
+
+            countDown.fillAmount = (currentTime / 10);
+
+            if (currentTime <= 3f)
+            {
+                StartCoroutine(boolSwitch());
+            }
+        }
+        else
+        {
+            timerText.color = red;
+            timerText.text = gameOver;
+        }
+       
+
+
+    }
+
+    IEnumerator boolSwitch()
+    {       
+
+        if (changeColour == true)
+        {
+            timerText.color = red;
+            countDown.color = white;
+            yield return new WaitForSeconds(.5f);
+            changeColour = false;
+        }
+        else
+        {
+            timerText.color = white;
+            countDown.color = red;
+            yield return new WaitForSeconds(.5f);
+            changeColour = true;
+        }       
+        StartCoroutine(boolSwitch());
+    }
+}
+
